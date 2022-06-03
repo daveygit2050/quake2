5ffe5a2799f6e2bb2988f5aab322a283e708260d
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2022-06-03 21:48:03 +0100

    Clipping, lava and skybox

--- compile.sh
+++ compile.sh
@@ -12,3 +12,6 @@ echo "Compiling ${map_name}.map to ${map_name}.bsp"
 ./tools/4rad -nopvs -chop -v ${map_name}.bsp
 
 cp ${map_name}.bsp ~/.steam/steam/steamapps/common/Quake\ 2/baseq2/maps
+
+/usr/games/steam -no-browser -applaunch 2320
+
